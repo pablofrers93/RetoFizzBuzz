af71d1701251f6378f58fd068d7521591dc65cd9
pablofrers93 <pablofrers93@gmail.com>
2023-05-11 18:26:38 -0300

    solved

---
+++ RetoFizzBuzz/RetoFizz_Buzz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoFizz_Buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[100];
+
+            SetNumeros(numeros);
+            FizzBuzz(numeros);
+            Console.ReadLine();
+        }
+
+        private static void FizzBuzz(int[] numeros)
+        {
+            foreach (var numero in numeros)
+            {
+                if (numero % 3 != 0 && numero % 5 != 0)
+                {
+                    Console.WriteLine($"{numero}");
+                }
+                else if (numero % 3 == 0 && numero % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (numero % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                    Console.WriteLine("buzz");
+            }
+        }
+
+        private static void SetNumeros(int[] numeros)
+        {
+            for (int i=0; i<100; i++)
+            {
+                numeros[i] = i+1;
+            }
+        }
+    }
+}
+ 
